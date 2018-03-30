a9409cae8b84ca10447c10a032b1e4bca5470d6c
jiangweiju <jiangweiju015@pingan.com.cn>
2018-03-30 15:17:30 +0800

    add normalize.css

--- .postcssrc.js
+++ .postcssrc.js
@@ -4,7 +4,6 @@ let path = require('path')
 module.exports = {
   plugins: {
     "postcss-import": {}, 
-    "postcss-url": {}, 
     "postcss-aspect-ratio-mini": {}, 
     "postcss-write-svg": { utf8: false }, 
     "autoprefixer": {
@@ -23,30 +22,5 @@ module.exports = {
     "postcss-viewport-units":{}, 
     "precss": {},
     "cssnano": { preset: "advanced", autoprefixer: false, "postcss-zindex": false }
-
-
-
-    // require('postcss-assets')({
-    //   loadPaths: ['**/*']
-
-    // }),
-    // require('postcss-pxtorem')({
-    //   rootValue: 16,
-    //   unitPrecision: 5,
-    //   propList: ['*', 
-    //'!height'],
-    //   selectorBlackList: [],
-    //   mediaQuery: false,
-    //   minPixelValue: 12
-    // }),
-
-
-    // require('postcss-import')({}),
-    // require('postcss-url')({}),
-    // require('autoprefixer')({
-    //   browsers: ['last 2 versions', '> 3%', 'safari >= 5', 'ie >= 10', 'opera >= 12', 'Firefox ESR', 'iOS >= 6', 'android >= 4']
-    // }),
-    // require('precss')({}),
-    // require('cssnano')({})
   }
 }
